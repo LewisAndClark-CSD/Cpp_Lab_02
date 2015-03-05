1f2af3e0d26bb0f57909ba93f3168b2784fdd8c2
unknown <IEUser@IE8Win7-15P.cis.com>
2015-03-05 06:20:59 -0800

    Here is my completed commit

---
+++ Lab02_3/Lab02_3/Lab02_3.cpp
@@ -0,0 +1,58 @@
+// Lab02_3.cpp : Defines the entry point for the console application.
+#include "stdafx.h"
+ #include <iostream>
+ #include <string>
+ #include <ctime>
+ #include <cstdlib>
+ using namespace std;
+
+int main()
+{
+	cout << "WELCOME to " << endl;
+	cout << "Die -Roller!" << endl;
+	cout << "*" << endl;
+	cout << "Instructions: *" << endl;
+	cout << "To roll, type the number of dice, then the number *" << endl;
+	cout << "of sides then any modifiers, all separated by *" << endl;
+	cout << "spaces. *" << endl;
+	cout << "*" << endl;
+	cout << "like this: 3 6 -1 *" << endl;
+	cout << "that would mean: *" << endl;
+	cout << "Roll 3 6-sided dice, and subtract 1 from each die *" << endl;
+	cout << "*" << endl;
+	cout << "To exit, enter: 1 1 1 *" << endl;
+	cout << "ENJOY! ********************************************************" << endl;
+	cout << endl;
+		//_____________________________________________________________________________________
+	
+		srand(time(0));
+	int dieNum = 0;
+	int dieSide = 0;
+	int dieMod = 0;
+	int sideRoll = 0;
+	int roll = 0;
+	int TotalRoll = 0;
+	
+		while (TotalRoll != 3){
+		cout << "Your Roll: ";
+		cin >> dieNum >> dieSide >> dieMod;
+		TotalRoll = dieNum + dieSide + dieMod;
+		if (TotalRoll > 3); {
+			for (int count = 0; count < dieNum; count++){
+				sideRoll = rand() % dieSide + 1;
+				
+					roll = roll + (sideRoll + dieMod);
+			}
+			cout << "You rolled " << dieNum << " " << dieSide << "-sided dice. " << dieMod << " was modified for each roll." << endl;
+			cout << "Total roll: " << TotalRoll << endl;
+			}
+		
+	}
+	
+	
+		getchar();
+	return 0;
+	}
+	
+	
+
